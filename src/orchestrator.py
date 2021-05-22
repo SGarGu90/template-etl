cfb98ed223949d8459a26d0c5a625344cc99db67
SGarGu90 <sgargu90@gmail.com>
2021-05-23 01:13:18 +0200

    Add default info logger for current orchestator config

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -5,5 +5,6 @@ def orchestrator(config, logger):
 
     try:
         logger.log.info("App orchestrator execution\n")
+        logger.log.info(f'App orchestrator arguments \'{config}\' \n')
     except Exception as err:
         logger.log_traceback(err)
